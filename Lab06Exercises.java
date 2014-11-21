9ccabd9b9ef511e3584f62c4a576ef96c0cc089b
Andre17 <andrepurits@Andre-Puritss-MacBook-Pro-2.local>
2014-11-21 14:58:31 +0000

    Lab06

--- Lab06Exercises.java
+++ Lab06Exercises.java
@@ -6,72 +6,83 @@
    {
        public static void main(String[] args) 
       {
-         String filename = JOptionPane.showInputDialog("What robot world?");
-         Display.openWorld("maps/" + filename + ".map");
+         Display.openWorld("maps/Lab06map.map");
          Display.setSize(10, 10);
          Display.setSpeed(10);
       
-         task_01(); //go to the end of the row of beepers
-         task_02(); //go to the beeper
-         task_03(); //go to the wall
-         task_04(); //go to the wall, pick up all the beepers (max one per pile)
-         task_05(); //go to the wall, pick up all the beepers
-         task_06(); //go to the end of the row of beepers, there is one gap
+         task_07(); //go to the beeper or the wall
+         task_08(); //go until you are next to another robot, then put all your beepers down
+         task_09(); //put down 5 different piles with 4 beepers in each pile
+         task_10(); //fill in gaps with a beeper and stop when you reach a wall
+         task_11(); //while there is a wall to your right put down one beeper at each step
+         task_12(); //go until there is a wall to your right and you are standing on a beeper
       }
-       public static void task_01()	
-      { //go to the end of the row of beepers
+      
+       public static void task_07()	
+      { //go to the beeper or the wall
          Robot temp = new Robot(1, 1, Display.EAST, 0);
-      int count = 0;
-      while(karel.nextToABeeper()){  
-      karel.pickBeeper();  
-      count++;}
-      System.out.println("Count is "+ count);
-             
-      public static void task_02()	
-      { //go to the beeper
-         Robot temp = new Robot(1, 2, Display.EAST, 0);
-      	while(!temp.nextToABeeper()) {
+         while(temp.frontIsClear() && !temp.nextToABeeper()) {
          temp.move(); }
       }
-       public static void task_03()	
-      { //go to the wall
-         Robot temp = new Robot(1, 3, Display.EAST, 0);
-         while(temp.frontIsClear()) {
+    
+      public static void task_08()	
+      { //go until you are next to another robot, then put all your beepers down
+         Robot andre = new Robot(4, 2, Display.EAST, 1);
+         Robot temp = new Robot(1, 2, Display.EAST, 1);
+         while(!temp.nextToARobot()) {
          temp.move(); }
+         while(temp.hasBeepers()) {
+         temp.putBeeper(); }
       }
-       public static void task_04()	
+                     
+       public static void task_09()	
+      { //put down 5 different piles with 4 beepers in each pile
+         Robot temp = new Robot(1, 3, Display.EAST, 20);
+               for(int n = 0; n<5; n++) {
+                  for(int k=0; k<4; k++) {
+                  temp.putBeeper(); 
+                  }
+                  temp.move();
+                  }
+      }
+              
+       public static void task_10()	
       { //go to the wall, pick up all the beepers (max one per pile)
-         Robot temp = new Robot(1, 4, Display.EAST, 0);
+         Robot temp = new Robot(1, 4, Display.EAST, 2);
          while(temp.frontIsClear()) {
-         while(temp.nextToABeeper()) {
-         temp.pickBeeper(); }
+            while(temp.nextToABeeper()) {
+               temp.move();
+            }
+            temp.putBeeper();
+            temp.move();
+            while(temp.nextToABeeper()) {
+               temp.move();
+            }
+            temp.putBeeper();
+            temp.move();
+         }            
+      }
+        
+       public static void task_11()	
+      { //while there is a wall to your right put down one beeper at each step
+         Robot temp = new Robot(1, 5, Display.EAST, Display.INFINITY);
+         while(!temp.rightIsClear()) {
+         temp.putBeeper();
          temp.move();
          }
-         if(!temp.frontIsClear() && temp.nextToABeeper()) {
-         temp.pickBeeper();
-      }
-      }
-       public static void task_05()	
-      { //go to the wall, pick up all the beepers
-         Robot temp = new Robot(1, 5, Display.EAST, 0);
-         while(temp.frontIsClear()) {
-         while(temp.nextToABeeper()) {
-         temp.pickBeeper(); }
-         temp.move();
-         }
-         while(!temp.frontIsClear() && temp.nextToABeeper()) {
-         temp.pickBeeper();
+        
       }
 
-      }
-       public static void task_06()
-      { //go to the end of the row of beepers, there is one gap
-         Robot temp = new Robot(1, 6, Display.EAST, 0);
-         while(temp.nextToABeeper()) {
-         temp.move(); }
-         if(temp.frontIsClear()) {
+     
+       public static void task_12() 
+      { //go until there is a wall to your right and you are standing on a beeper 
+      Robot temp = new Robot(1, 6, Display.EAST, 0);
+         while(!temp.nextToABeeper() || temp.rightIsClear()) {
          temp.move();
-         while(temp.nextToABeeper()) {
-         temp.move(); } }
       }
    }
+}
+
+
+
+
